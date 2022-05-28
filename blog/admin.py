967cd36406edaa81271c781fe7064d6d1497b29d
vfg-95 <sam.ridding@hotmail.co.uk>
2022-05-28 14:43:31 +0000

    Register models for admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models import Post, Release
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'posted_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'posted_on')
+
+
+@admin.register(Release)
+class ReleaseAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'cover_img')
+
