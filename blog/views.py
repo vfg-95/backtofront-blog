e0fb161920932a981c79dde593b09414adc1e63b
vfg-95 <sam.ridding@hotmail.co.uk>
2022-05-29 14:39:13 +0000

    Create individual view and url for individual blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -7,3 +7,18 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-posted_on")
     template_name = "index.html"
+
+
+class BlogPost(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            "blog/blog_post.html",
+            {
+                "post": post,
+            },
+        )
