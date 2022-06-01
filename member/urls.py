1023747a24c37332c6bd255c5dde79dc03eddf7b
vfg-95 <sam.ridding@hotmail.co.uk>
2022-06-01 15:14:02 +0000

    Delete memeber views and urls

--- member/urls.py
+++ member/urls.py
@@ -1,10 +1,2 @@
 from django.urls import path
 from . import views
-# from .views import MemberSignUp
-
-
-urlpatterns = [
-    path('login/', views.UserLogin, name='userlogin'),
-    path('logout/', views.UserLogOut, name='userlogout'),
-    # path('signup/', MemeberSignUp.as_view(), name='signup'),
-]
